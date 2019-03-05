fa3b5f2f83638a02083522c279ff614c39dd4742
Satyan <msatyan@gmail.com>
2019-03-04 23:40:27 -0600

    doc

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Defining foutes
+// Defining routes
 app.use('/', rt_index);
 app.use('/v1/order', rt_order);
 app.use('/v1/product', rt_product);
